d048031cfad5441ac56a867534bc3aca5cfdfca5
eisverticker <eisverticker@posteo.de>
2018-05-16 23:06:30 +0200

    add index file

--- src/fussball-html.service.ts
+++ src/fussball-html.service.ts
@@ -4,22 +4,7 @@ import { HttpClient, HttpClientResponse } from 'typed-rest-client/HttpClient';
 
 import { Matchplan } from './matchplan';
 import { Month } from './month';
-
-export type MimeType = "JSON";
-export type Mode = "PAGE";
-
-export class MatchplanUrlOptions {
-    public mimeType: MimeType = "JSON";
-    public mode: Mode = "PAGE";
-    public maxNumOfEntries = 10;
-    public dateFrom: Date = new Date();
-    public dateTo: Date = new Date();
-    public entryOffset = 0;
-    public isShowFilter = false;
-    public isShowVenues = true;
-    public isShowLegend = false;
-    public isShowTabs = false;
-}
+import { MatchplanUrlOptions } from './matchplan-url-options';
 
 export class FussballHtmlService {
   static requestEntryLimit = 200;
---
+++ src/matchplan-url-options.ts
@@ -0,0 +1,15 @@
+export type MimeType = "JSON";
+export type Mode = "PAGE";
+
+export class MatchplanUrlOptions {
+    public mimeType: MimeType = "JSON";
+    public mode: Mode = "PAGE";
+    public maxNumOfEntries = 10;
+    public dateFrom: Date = new Date();
+    public dateTo: Date = new Date();
+    public entryOffset = 0;
+    public isShowFilter = false;
+    public isShowVenues = true;
+    public isShowLegend = false;
+    public isShowTabs = false;
+}
